0da7dad1c9edf420aa40a88a1de980d5a2c340f5
reaper8055 <11490705+reaper8055@users.noreply.github.com>
2024-03-07 01:42:40 +0530

    barebone for realtime DB in go

--- shell.nix
+++ shell.nix
@@ -20,7 +20,7 @@ mkShell {
   ];
   shellHook = ''
     export GIT_CONFIG_NOSYSTEM=true
-    export GIT_CONFIG_SYSTEM="/home/grim_reaper/Projects/configs/github/github_global"
-    export GIT_CONFIG_GLOBAL="/home/grim_reaper/Projects/configs/github/github_global"
+    export GIT_CONFIG_SYSTEM="/home/grim_reaper/Projects/configs/github_global"
+    export GIT_CONFIG_GLOBAL="/home/grim_reaper/Projects/configs/github_global"
   '';
 }
